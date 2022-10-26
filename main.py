6056b45acea15888111a6cc8ea9bf97f65d606ea
Tahmid Alavi Ishmam <31489330+Tahmid1999@users.noreply.github.com>
2022-10-26 23:53:24 +0600

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ import random
 import pickle
 import re, itertools
 from lxml import etree
+from selenium.webdriver.common.keys import Keys
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -39,10 +40,13 @@ options.add_argument("--disable-notifications")
 
 insta_email = input("Enter your instagram email: ")
 insta_password = input("Enter your  instagram password: ")
-f = open("message.txt", "r")
+f = open("message.txt", "r", encoding="utf-8")
 my_message = f.read()
 
 
+
+already_sent_message = []
+count = 0
 
 def xpath_soup(element):
     components = []
@@ -58,10 +62,17 @@ def xpath_soup(element):
     components.reverse()
     return '/%s' % '/'.join(components)
 
+with open('already_send_message.csv', 'r', encoding="utf-8") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        if "/" in row[0]:
+            already_sent_message.append(row[0])
+
+
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)  # version_main allows to specify your chrome version instead of following chrome global version
 driver.maximize_window()
 driver.get("https://www.instagram.com/accounts/login/")
-time.sleep(2)
+time.sleep(10)
 
 
 driver.find_element(By.NAME, 'username').send_keys(insta_email)
@@ -69,16 +80,18 @@ driver.find_element(By.NAME, 'password').send_keys(insta_password)
 
         
 driver.find_element(By.XPATH, "//button[@type='submit']").click()
-WebDriverWait(driver, 60000).until(EC.visibility_of_element_located((By.XPATH, "//*[contains(@id,'mount')]")))
 
+time.sleep(10)
+WebDriverWait(driver, 60000).until(EC.visibility_of_element_located((By.XPATH, "//*[contains(@id,'mount')]")))
+driver.get("https://www.instagram.com")
+time.sleep(3)
 with open('profile_links.csv', 'r', encoding="utf-8") as file:
     reader = csv.reader(file)
     for row in reader:
-        if '/' in row[0]:
+        if '/' in row[0] and row[0] not in already_sent_message:
             print(row[0])
-            driver.get(row[0])
-            time.sleep(2)
-            WebDriverWait(driver, 60000).until(EC.visibility_of_element_located((By.XPATH, "//*[contains(@id,'mount')]")))
+            driver.get("https://www.instagram.com"+row[0])
+            time.sleep(15)
             html = driver.page_source
             soup = BeautifulSoup(html, features="html.parser")
 
@@ -91,13 +104,32 @@ with open('profile_links.csv', 'r', encoding="utf-8") as file:
                 if i.text == 'Message':
                     my_xpath = str(xpath_soup(i))
                     driver.find_element(By.XPATH, my_xpath).click()
-                    # pyperclip.copy(my_message)
-                    time.sleep(5)
-                    pyautogui.write(my_message)
-                    time.sleep(3)
-                    pyautogui.press('enter')
-                    time.sleep(3)
-                    break
+                    time.sleep(10)
+                    try:
+                        driver.find_element(By.TAG_NAME, "textarea").send_keys(my_message)
+                        time.sleep(10)
+                        # pyautogui.press('enter')
+                        driver.find_element(By.TAG_NAME, "textarea").send_keys(Keys.RETURN)
+
+                        time.sleep(3)
+                        already_sent_message.append(row[0])
+                        already_sent_message_np = np.array(already_sent_message)
+                        already_sent_message_pamdas = pd.DataFrame({"Profile Link" : already_sent_message_np})
+                        already_sent_message_pamdas.to_csv("already_send_message.csv", index=False)  
+                        count = count +1
+
+                        break
+                    except:
+                        print("Error occured----")
+                else:
+                    already_sent_message.append(row[0])
+                    already_sent_message_np = np.array(already_sent_message)
+                    already_sent_message_pamdas = pd.DataFrame({"Profile Link" : already_sent_message_np})
+                    already_sent_message_pamdas.to_csv("already_send_message.csv", index=False)  
+
+        print(count)    
+        if count == 40:
+            break
 
 
 
