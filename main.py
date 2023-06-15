2595ae45a4c32790eede9527b3a68e9e52a02019
Tahmid Alavi Ishmam <31489330+Tahmid1999@users.noreply.github.com>
2023-06-15 14:25:40 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from xml.dom.minidom import Document
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
@@ -30,16 +32,18 @@ import os
 import random
 import pickle
 import re, itertools
-from lxml import etree
 from selenium.webdriver.common.keys import Keys
-
-options = webdriver.ChromeOptions()
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-options.add_argument("--disable-notifications")
+import numpy as np
+from selenium.webdriver.common.action_chains import ActionChains
 
 
-insta_email = input("Enter your instagram email: ")
-insta_password = input("Enter your  instagram password: ")
+# options = webdriver.ChromeOptions()
+# options.add_experimental_option('excludeSwitches', ['enable-logging'])
+# options.add_argument("--disable-notifications")
+
+
+insta_email = "Instagram email"
+insta_password = "Instagram password"
 f = open("message.txt", "r", encoding="utf-8")
 my_message = f.read()
 
@@ -65,11 +69,16 @@ def xpath_soup(element):
 with open('already_send_message.csv', 'r', encoding="utf-8") as file:
     reader = csv.reader(file)
     for row in reader:
-        if "/" in row[0]:
+        try:
             already_sent_message.append(row[0])
+        except:
+            print("Error")
 
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)  # version_main allows to specify your chrome version instead of following chrome global version
+#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)  # version_main allows to specify your chrome version instead of following chrome global version
+
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
 driver.maximize_window()
 driver.get("https://www.instagram.com/accounts/login/")
 time.sleep(10)
@@ -82,15 +91,18 @@ driver.find_element(By.NAME, 'password').send_keys(insta_password)
 driver.find_element(By.XPATH, "//button[@type='submit']").click()
 
 time.sleep(10)
+
+input("Enter c to continue :")
 WebDriverWait(driver, 60000).until(EC.visibility_of_element_located((By.XPATH, "//*[contains(@id,'mount')]")))
 driver.get("https://www.instagram.com")
 time.sleep(3)
 with open('profile_links.csv', 'r', encoding="utf-8") as file:
     reader = csv.reader(file)
     for row in reader:
-        if '/' in row[0] and row[0] not in already_sent_message:
+        if row[0] not in already_sent_message:
+            print("Profile  link => ")
             print(row[0])
-            driver.get("https://www.instagram.com"+row[0])
+            driver.get("https://www.instagram.com/"+row[0])
             time.sleep(15)
             html = driver.page_source
             soup = BeautifulSoup(html, features="html.parser")
@@ -137,11 +149,6 @@ with open('profile_links.csv', 'r', encoding="utf-8") as file:
         print(count)    
         if count == 10000:
             break
-
-
-
-
-
 
 
 
